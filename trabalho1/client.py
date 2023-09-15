68da67b80686aa896e83eddf0be5b45b5e7a0e7d
MarcValentino <mvalentino@id.uff.br>
2023-09-15 13:44:46 -0300

    full code for the first assignment

--- trabalho1/client.py
+++ trabalho1/client.py
@@ -1,17 +1,32 @@
 import rpyc
 import sys
+from time import time
 
 def create_array(length):
    return list(i for i in range(length))
 
-if len(sys.argv) < 2:
-   exit("Usage {} SERVER".format(sys.argv[0]))
- 
-server = sys.argv[1]
- 
-connection = rpyc.connect(server,18861)
+def call_exposed_function_and_variable(connection):
+   print(connection.root.get_answer())
+   print(connection.root.the_real_answer_though)
 
-print(connection.root)
-print(connection.root.sum_array(create_array(10000))) #chamada pro método de somar um array
-print(connection.root.get_answer())
-print(connection.root.the_real_answer_though)
+def try_unexposed_function(connection):
+   print(connection.root.get_question())
+
+def call_arraysum_function(connection, length):
+   start = time()
+   array = create_array(length)
+   print(connection.root.sum_array(array))
+   end = time()
+   print("Tempo de execução no cliente: {}s".format(end-start))
+
+if __name__ == "__main__":
+   if len(sys.argv) < 2:
+      exit("Usage {} SERVER".format(sys.argv[0]))
+   
+   server = sys.argv[1]
+   
+   connection = rpyc.connect(server,18861)
+   # call_exposed_function_and_variable(connection)
+   try_unexposed_function(connection)
+   # call_arraysum_function(connection, 10000)
+   
--- trabalho1/server.py
+++ trabalho1/server.py
@@ -1,5 +1,6 @@
 import rpyc
 from functools import reduce
+from time import time
 class MyService(rpyc.Service):
     def on_connect(self, conn):
         # código que é executado quando uma conexão é iniciada, caso seja necessário
@@ -10,7 +11,11 @@ class MyService(rpyc.Service):
     def exposed_get_answer(self): # este é um método exposto
         return 42
     def exposed_sum_array(self, array): # já implementei a função de soma de um array rs
-        return reduce(lambda x, y: x+y, array)
+        start = time()
+        result = reduce(lambda x, y: x+y, array)
+        end = time()
+        print("Tempo de execução no servidor: {}s".format(end-start))
+        return result
     exposed_the_real_answer_though = 43     # este é um atributo exposto
     def get_question(self):  # este método não é exposto
         return "Qual é  a cor do cavalo branco de Napoleão?"
